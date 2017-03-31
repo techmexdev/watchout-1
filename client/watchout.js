afdbf803032488396f3c616f7b9770f8a287fcfc
Fliko <baxter.flw@gmail.com>
2017-03-31 17:36:27 -0500

    Make a player that is draggable

--- client/watchout.js
+++ client/watchout.js
@@ -39,13 +39,43 @@ var update = function(data) {
   
   enemies.exit().remove();
 };
+var player = function() {
+  var array = [{id: 'player', x: 50, y: 50}];
 
+  var shape = d3.select('svg').selectAll('rect')
+                .data(array, function(d) {
+                  return d.id;
+                });
+
+  shape.enter().append('rect')
+        .attr('x', (d) => axes.x(50))
+        .attr('y', (d) => axes.y(50))
+        .attr('fill', 'red')
+        .attr('width', '15')
+        .attr('height', '15')
+        .attr('stroke', 'blue')
+        .attr('stroke-width', '2')
+        .attr('transform', 'matrix(1 0 0 1 0 0)')
+        .attr('onmousedown', 'selectElement(evt)');
+};
+
+// transform="translate(50,80)" 
+
+player();
 var asteroids = makeEnemy();
 update(asteroids);
 
-setInterval(function(){
+setInterval(function() {
   var ast = makeEnemy();
   update(ast);
 }, 1000);
 
-
+/// player polygon
+  /*shape.enter().append('polygon')
+        .attr('points', '35,7.5  37.9,16.1 46.9,16.1 39.7,21.5, 42.3,30.1 35,25 27.7,30.1 30.3,21.5, 23.1,16.1 32.1,16.1')
+        .attr('fill', 'red')
+        // .attr('cx', (d) => axes.x(d.x))
+        // .attr('cy', (d) => axes.y(d.y))
+        .attr('stroke', 'blue')
+        .attr('stroke-width', '2')
+        .attr('transform' , 'matrix(1 0 0 1 0 0)');*/
